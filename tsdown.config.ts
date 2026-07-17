347fe4bd13c35ae48bb8573b6cfd050d1924b3ca
big-yellow-dog <129140504+Kleindevops@users.noreply.github.com>
2026-07-17 09:59:16 +0800

    Update tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,26 +1,12 @@
 import { defineConfig } from 'tsdown';
 
-const namespaceOf = (id: string) => id.match(/[\\/]lib[\\/]routes[\\/]([^\\/]+)[\\/]/)?.[1];
-
 export default defineConfig({
-    entry: ['./lib/index.ts'],
+    entry: ['./lib/server.ts'],
+    outDir: 'dist',
     minify: true,
     shims: true,
     clean: true,
-    copy: ['lib/assets'],
     deps: {
         onlyBundle: false,
-    },
-    outputOptions: {
-        chunkFileNames(chunk) {
-            let namespace = chunk.facadeModuleId ? namespaceOf(chunk.facadeModuleId) : undefined;
-            if (!namespace) {
-                const namespaces = new Set(chunk.moduleIds.map((id) => namespaceOf(id)));
-                if (namespaces.size === 1) {
-                    namespace = [...namespaces][0];
-                }
-            }
-            return namespace && namespace !== chunk.name ? `${namespace}-[name]-[hash].mjs` : '[name]-[hash].mjs';
-        },
     },
 });
