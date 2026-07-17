8ed006c2ffd03a7c2764a5efc51776814f14f83b
big-yellow-dog <129140504+Kleindevops@users.noreply.github.com>
2026-07-17 09:05:24 +0800

    Update tsdown-vercel.config.ts

--- tsdown-vercel.config.ts
+++ tsdown-vercel.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
     entry: ['./lib/server.ts'],
-    // Wait for https://github.com/vercel/vercel/pull/14429
-    // Then we can set outDir to outputDirectory in vercel.json
-    outDir: 'src',
+    outDir: '.',           // 改成根目录（重要！）
     minify: true,
     shims: true,
     clean: true,
-    // copy: [{ from: 'lib/assets', to: 'dist' }],
     deps: {
         onlyBundle: false,
     },
